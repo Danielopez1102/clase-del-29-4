4cc5cb13c5670f0372c156fe7e50f3b02f9f17ed
Danielopez1102 <daniel.espinoza.lopez@academiacuatropuntocero.com>
2022-05-03 22:02:52 -0600

    clase del 29-4

---
+++ index.js
@@ -0,0 +1,26 @@
+alerta ('Esta funcionando');
+
+// 1
+
+var numero_que_queremos_evaluar = 100;
+
+function log10(numero) {
+ return math.log10(numero);
+}
+consola. log(log10(numero_que_queremos_evaluar));
+
+// 2
+
+ var text = 'mañana vamos a comer un hamburguesa, y luego  vamos al cine'
+
+function busqueda_del_cine(texto) {
+ if (texto. incluye('cine')) {
+ if (texto. includes('no')) {
+ consola. log('no vamos a ir al cine');
+      }else{
+ consola. log('vamos a ir al cine');
+      }  
+    }
+}
+
+busqueda_del_cine (texto); 
